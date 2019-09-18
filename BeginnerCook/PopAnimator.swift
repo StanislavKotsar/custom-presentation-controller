80793062a0e09d8cddad38883ce4058f5ece58a7
Stanislav Kotsar <minolin27@gmail.com>
2019-09-18 21:44:44 +0300

    add UIViewControllerTransitioningDelegate and animator object

---
+++ BeginnerCook/PopAnimator.swift
@@ -0,0 +1,21 @@
+//
+//  PopAnimator.swift
+//  BeginnerCook
+//
+//  Created by Станислав Коцарь on 18/09/2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    
+}
